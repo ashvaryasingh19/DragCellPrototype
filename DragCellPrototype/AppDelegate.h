5ee079106c16f502bc2480f574dad6cd73c193f0
Ashwarya Singh <ashwarya.singh@Mac133.local>
2014-07-30 11:47:58 +0530

    Initial Commit

---
+++ DragCellPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DragCellPrototype
+//
+//  Created by Ashwarya Singh on 7/30/14.
+//  Copyright (c) 2014 A1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DragCellPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragCellPrototype
+//
+//  Created by Ashwarya Singh on 7/30/14.
+//  Copyright (c) 2014 A1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
